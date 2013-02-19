72a00d8a1f45623d02bc1f70218eaf0f53a15d66
Denny Winoto <denny.winoto@gmail.com>
2013-02-19 14:20:27 -0800

    iOS frontent

---
+++ iOS LoginCounter/LoginCounter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LoginCounter
+//
+//  Created by Denny Winoto on 2/18/13.
+//  Copyright (c) 2013 Denny Winoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS LoginCounter/LoginCounter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LoginCounter
+//
+//  Created by Denny Winoto on 2/18/13.
+//  Copyright (c) 2013 Denny Winoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
