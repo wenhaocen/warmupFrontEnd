8ee73a2bc5011364cb92a1ba9c5c98c08db97852
Wenhao Cen <wenhaocen@Wenhaos-MacBook-Pro.local>
2013-02-19 21:43:49 -0800

    removing the old file

---
+++ Bing's/Bing's/WarmUp4AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WarmUp4AppDelegate.h
+//  Bing's
+//
+//  Created by Wenhao Cen on 2/19/13.
+//  Copyright (c) 2013 Wenhao Cen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WarmUp4AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Bing's/Bing's/WarmUp4ViewController.m
@@ -0,0 +1,29 @@
+//
+//  WarmUp4ViewController.m
+//  Bing's
+//
+//  Created by Wenhao Cen on 2/19/13.
+//  Copyright (c) 2013 Wenhao Cen. All rights reserved.
+//
+
+#import "WarmUp4ViewController.h"
+
+@interface WarmUp4ViewController ()
+
+@end
+
+@implementation WarmUp4ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
